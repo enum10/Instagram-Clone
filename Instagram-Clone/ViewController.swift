2064700c56001b7594babb96ccb16daee9e8f0e6
Inam Ahmad-zada <onedev@Inams-MacBook-Pro.local>
2018-01-08 16:32:44 -0500

    Added input field: Textfields and Sign up Button

--- Instagram-Clone/ViewController.swift
+++ Instagram-Clone/ViewController.swift
@@ -16,6 +16,47 @@ class ViewController: UIViewController {
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
+    
+    let emailTextField: UITextField = {
+        let view = UITextField()
+        view.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        view.borderStyle = .roundedRect
+        view.font = UIFont.systemFont(ofSize: 14)
+        view.placeholder = "Email"
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let usernameTextField: UITextField = {
+        let view = UITextField()
+        view.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        view.borderStyle = .roundedRect
+        view.font = UIFont.systemFont(ofSize: 14)
+        view.placeholder = "Username"
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let passwordTextField: UITextField = {
+        let view = UITextField()
+        view.isSecureTextEntry = true
+        view.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        view.borderStyle = .roundedRect
+        view.font = UIFont.systemFont(ofSize: 14)
+        view.placeholder = "Password"
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let signUpButton: UIButton = {
+        let view = UIButton(type: .system)
+        view.backgroundColor = UIColor(red: 149/255, green: 204/255, blue: 244/255, alpha: 1)
+        view.setTitle("Sign up", for: .normal)
+        view.setTitleColor(.white, for: .normal)
+        view.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        view.layer.cornerRadius = 5
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +66,24 @@ class ViewController: UIViewController {
             plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             plusPhotoButton.widthAnchor.constraint(equalToConstant: 140),
             plusPhotoButton.heightAnchor.constraint(equalToConstant: 140)
+            ])
+        
+        setupInputFields()
+    }
+    
+    func setupInputFields() {
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, usernameTextField, passwordTextField, signUpButton])
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: plusPhotoButton.bottomAnchor, constant: 20),
+            stackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40),
+            stackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40),
+            stackView.heightAnchor.constraint(equalToConstant: 200)
             ])
     }
 }
