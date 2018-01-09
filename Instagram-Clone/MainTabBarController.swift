328e45fa4942cd3cc388f724661bca2352b6790e
Inam Ahmad-zada <onedev@Inams-MacBook-Pro.local>
2018-01-08 19:23:49 -0500

    Added user profile controller into tab bar controller

--- Instagram-Clone/MainTabBarController.swift
+++ Instagram-Clone/MainTabBarController.swift
@@ -10,4 +10,18 @@ import UIKit
 
 class MainTabBarController: UITabBarController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let layout = UICollectionViewFlowLayout()
+        let userProfileController = UserProfileController(collectionViewLayout: layout)
+        let navController = UINavigationController(rootViewController: userProfileController)
+        
+        navController.tabBarItem.image = #imageLiteral(resourceName: "profile_unselected")
+        navController.tabBarItem.selectedImage = #imageLiteral(resourceName: "profile_unselected")
+        
+        tabBar.tintColor = .black
+        
+        viewControllers = [navController]
+    }
 }
