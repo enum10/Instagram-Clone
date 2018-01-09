e4eaa62a1ffe217ef00fe9cd09ac3a40767c05e5
Inam Ahmad-zada <onedev@Inams-MacBook-Pro.local>
2018-01-08 19:20:44 -0500

    Created MainTabBarController and UserProfileController

---
+++ Instagram-Clone/MainTabBarController.swift
@@ -0,0 +1,13 @@
+//
+//  MainTabBarController.swift
+//  Instagram-Clone
+//
+//  Created by Inam Ahmad-zada on 2018-01-08.
+//  Copyright © 2018 Inam Ahmad-zada. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+}
