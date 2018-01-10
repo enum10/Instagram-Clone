6887c908380bbf7d1e7017e0890ab16e13729c29
Inam Ahmad-zada <inam.ahmadzada@outlook.com>
2018-01-10 00:25:15 -0500

    Added logo container view at the top of login controller

--- Instagram-Clone/Login/LoginController.swift
+++ Instagram-Clone/Login/LoginController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class LoginController: UIViewController {
     
+    let logoContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.rgb(red: 0, green: 120, blue: 175)
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "Instagram_logo_white"))
+        imageView.contentMode = .scaleAspectFill
+        view.addSubview(imageView)
+        imageView.anchor(top: nil, left: nil, bottom: nil, right: nil, topPadding: 0, leftPadding: 0, bottomPadding: 0, rightPadding: 0, width: 200, height: 50)
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        return view
+    }()
+    
     let signUpButton: UIButton = {
         let view = UIButton(type: .system)
         view.setTitle("Don't have an account? Sign up", for: .normal)
@@ -17,12 +29,18 @@ class LoginController: UIViewController {
         return view
     }()
     
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationController?.isNavigationBarHidden = true
         
         view.backgroundColor = .white
+        view.addSubview(logoContainerView)
+        logoContainerView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, topPadding: 0, leftPadding: 0, bottomPadding: 0, rightPadding: 0, width: 0, height: 180)
         view.addSubview(signUpButton)
         signUpButton.anchor(top: nil, left: view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor, topPadding: 0, leftPadding: 0, bottomPadding: 0, rightPadding: 0, width: 0, height: 50)
     }
