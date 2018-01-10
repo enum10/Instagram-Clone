057435e160f3f25049c8110f4c1bd34e70ecf472
Inam Ahmad-zada <inam.ahmadzada@outlook.com>
2018-01-09 23:45:38 -0500

    Created LoginController class

---
+++ Instagram-Clone/Login/LoginController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginController.swift
+//  Instagram-Clone
+//
+//  Created by Inam Ahmad-zada on 2018-01-09.
+//  Copyright © 2018 Inam Ahmad-zada. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
