ac291a6ca9f88692d2cb7a8d731f88504a224161
Inam Ahmad-zada <inam.ahmadzada@outlook.com>
2018-01-10 14:20:27 -0500

    Created SharePhotoController

---
+++ Instagram-Clone/PhotoSelection/SharePhotoController.swift
@@ -0,0 +1,17 @@
+//
+//  SharePhotoController.swift
+//  Instagram-Clone
+//
+//  Created by Inam Ahmad-zada on 2018-01-10.
+//  Copyright © 2018 Inam Ahmad-zada. All rights reserved.
+//
+
+import UIKit
+
+class SharePhotoController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
