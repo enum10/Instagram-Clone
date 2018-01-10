a45027ceabe74816464d63ef827ef3670c12b222
Inam Ahmad-zada <inam.ahmadzada@outlook.com>
2018-01-10 12:31:53 -0500

    Navigation bar items in photo selector

--- Instagram-Clone/PhotoSelection/PhotoSelectorController.swift
+++ Instagram-Clone/PhotoSelection/PhotoSelectorController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class PhotoSelectorController: UICollectionViewController {
     
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView?.backgroundColor = .yellow
+        
+        setupNavigationBarButtons()
+    }
+    
+    fileprivate func setupNavigationBarButtons() {
+        navigationController?.navigationBar.tintColor = .black
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelAction))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action: #selector(nextAction))
+    }
+    
+    @objc
+    func cancelAction() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc
+    func nextAction() {
+        print("Next")
     }
 }
