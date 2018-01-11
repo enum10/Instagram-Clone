156ff63917496affb592e6f77eef58c82575a65f
Inam Ahmad-zada <inam.ahmadzada@outlook.com>
2018-01-10 20:39:55 -0500

    HomeController and HomePostCell

---
+++ Instagram-Clone/Home/HomeController.swift
@@ -0,0 +1,33 @@
+//
+//  HomeController.swift
+//  Instagram-Clone
+//
+//  Created by Inam Ahmad-zada on 2018-01-10.
+//  Copyright © 2018 Inam Ahmad-zada. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    let cellId = "cellId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = .white
+        collectionView?.register(HomePostCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! HomePostCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.size.width, height: 200)
+    }
+}
---
+++ Instagram-Clone/Home/HomePostCell.swift
@@ -0,0 +1,21 @@
+//
+//  HomePostCell.swift
+//  Instagram-Clone
+//
+//  Created by Inam Ahmad-zada on 2018-01-10.
+//  Copyright © 2018 Inam Ahmad-zada. All rights reserved.
+//
+
+import UIKit
+
+class HomePostCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .blue
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
