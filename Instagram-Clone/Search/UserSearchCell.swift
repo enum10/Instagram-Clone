d4bad9fd3b42718064a351e638732e2efd0173d2
Inam Ahmad-zada <inam.ahmadzada@outlook.com>
2018-01-13 13:46:07 -0500

    Added temporary fields of User search cell

--- Instagram-Clone/Search/UserSearchCell.swift
+++ Instagram-Clone/Search/UserSearchCell.swift
@@ -6,4 +6,36 @@
 //  Copyright © 2018 Inam Ahmad-zada. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class UserSearchCell: UICollectionViewCell {
+    
+    let userImageView: CustomImageView = {
+        let view = CustomImageView()
+        view.contentMode = .scaleAspectFill
+        view.clipsToBounds = true
+        view.backgroundColor = UIColor.rgb(red: 240, green: 240, blue: 240)
+        return view
+    }()
+    
+    let usernameLabel: UILabel = {
+        let view = UILabel()
+        view.text = "Username"
+        view.font = UIFont.boldSystemFont(ofSize: 14)
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .yellow
+        addSubview(userImageView)
+        addSubview(usernameLabel)
+        userImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: nil, topPadding: 8, leftPadding: 8, bottomPadding: 8, rightPadding: 0, width: 50, height: 0)
+        userImageView.layer.cornerRadius = 50 / 2
+        usernameLabel.anchor(top: userImageView.topAnchor, left: userImageView.rightAnchor, bottom: userImageView.bottomAnchor, right: rightAnchor, topPadding: 0, leftPadding: 8, bottomPadding: 0, rightPadding: 8, width: 0, height: 0)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
